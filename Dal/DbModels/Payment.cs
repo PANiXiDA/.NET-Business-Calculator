031f5374908eb065a46e168116a1ff4f3b7da41c
Михаил <mixaila17may2003@gmail.com>
2024-05-07 22:13:08 +0400

    Создал шаблон с админкой

---
+++ Dal/DbModels/Payment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dal.DbModels;
+
+public partial class Payment
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public DateTime PaymentDate { get; set; }
+
+    public string PaymentMethod { get; set; }
+
+    public string Status { get; set; }
+
+    public string Description { get; set; }
+}
